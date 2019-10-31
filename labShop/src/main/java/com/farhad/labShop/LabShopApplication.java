afd9fe885cbe04f157d4cf97db90eb86ce7726e3
farhadc <f.t.farahmand@gmail.com>
2019-10-31 08:40:13 +0330

    start the project

---
+++ labShop/src/main/java/com/farhad/labShop/LabShopApplication.java
@@ -0,0 +1,13 @@
+package com.farhad.labShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabShopApplication.class, args);
+	}
+
+}
---
+++ labShop/src/test/java/com/farhad/labShop/LabShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhad.labShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
