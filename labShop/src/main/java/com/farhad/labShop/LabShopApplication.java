40c019176588e349ac3af567ea41ae79b8f0f61a
farhadc <f.t.farahmand@gmail.com>
2019-11-01 10:21:14 +0330

    database , repository and service is initialized

--- labShop/src/main/java/com/farhad/labShop/LabShopApplication.java
+++ labShop/src/main/java/com/farhad/labShop/LabShopApplication.java
@@ -1,13 +1,26 @@
 package com.farhad.labShop;
 
+import com.farhad.labShop.Service.AddNewOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+
 public class LabShopApplication {
+	@Autowired
+	AddNewOrderService orderService;
+
+	@PostConstruct
+	public void init(){
+		orderService.setOrder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LabShopApplication.class, args);
+
 	}
 
 }
